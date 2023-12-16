e2ac13fd248c869edc419f75702786fb740eaf4c
peacepiece7 <peacepiece7@naver.com>
2023-12-17 08:00:48 +0900

    test : 환경 변수 추가

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -14,19 +14,25 @@ describe('렌딩 페이지', () => {
   })
 
   it('5개 이상의 최신 포스트가 화면에 출력됩니다.', () => {
-    cy.should('exist', '#postList')
-    cy.get('#postList').children().should('have.length.greaterThan', 4)
+    cy.get('#postList').should('exist').children().should('have.length.greaterThan', 4)
     cy.get('#postList').children().first().should('contain', 'Created At')
     cy.get('#postList').children().first().should('contain', 'Tags')
   })
 
   it('포스트를 클릭하면 상세 페이지로 이동합니다.', () => {
-    cy.should('exist', '#postList')
-    cy.get('#postList').children().first().click()
+    cy.get('#postList').should('exist').children().first().click()
     cy.url().should('include', '/log/')
+
     // * 로그 제목과 TOC가 존재해야합니다.
     cy.get('h1').should('exist')
     cy.contains('Table of Contents')
+
+    // * title 태그에 포스트의 제목(h1)이 포함되어야 합니다.
+    cy.should('exist', 'h1')
+    cy.get('h1')
+      .first()
+      .invoke('text')
+      .then((h1Text) => cy.title().should('include', h1Text))
   })
 
   it('네비게이션에서 Log를 클릭하면 "/logs" 페이지로 이동합니다.', () => {
@@ -43,25 +49,5 @@ describe('렌딩 페이지', () => {
     cy.get('h1')
       .invoke('text')
       .then((h1Text) => cy.wrap(h1Text.toLowerCase()).should('include', 'wiki'))
-  })
-})
-
-describe('포스트 상세 보기 페이지', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.get('#postList').children().first().click()
-  })
-
-  it('title 태그에 포스트의 제목(h1)이 포함되어야 합니다.', () => {
-    cy.should('exist', 'h1')
-    cy.get('h1')
-      .first()
-      .invoke('text')
-      .then((h1Text) => cy.title().should('include', h1Text))
-  })
-
-  it('로그 제목과 TOC가 존재해야합니다.', () => {
-    cy.get('h1').should('exist')
-    cy.contains('Table of Contents')
   })
 })
